acd45503311dc307ab5a46e03ec78f30815fa1d9
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-11-26 21:39:23 +0700

    dung-set up the back-end for the project

---
+++ online-tutor-finding/src/main/java/com/database241/onlinetutorfinding/OnlineTutorFindingApplication.java
@@ -0,0 +1,13 @@
+package com.database241.onlinetutorfinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTutorFindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTutorFindingApplication.class, args);
+	}
+
+}
---
+++ online-tutor-finding/src/test/java/com/database241/onlinetutorfinding/OnlineTutorFindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database241.onlinetutorfinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTutorFindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
